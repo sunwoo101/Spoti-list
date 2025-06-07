cef83cb46ccdd45446633fff407cd163f508c63f
Daniel Liu <daniel.liu8750@gmail.com>
2025-06-08 01:53:40 +1000

    Initial commit: React + Node.js setup

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🎧 Spoti-List 🎵</h1>
+      <p>Personal Spotify playlist data tracker.</p>
+    </div>
+  );
+}
+
+export default App
