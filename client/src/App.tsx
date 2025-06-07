d4fa9e9a3afbeac41e532ba854bca4c67ae3c22f
Daniel Liu <daniel.liu8750@gmail.com>
2025-06-08 04:20:49 +1000

    Update App.tsx
    Removed CLIENT_ID, REDIRECTED_LINK and scope from frontend. Relocated them to backend auth.js file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,8 @@
 import './App.css'
 
-const CLIENT_ID = '56f9ce77e267463a93248740284844e9';
-const REDIRECT_LINK = 'https://witty-plant-067c6f61e.6.azurestaticapps.net/callback';
-const SCOPE = 'playlist-read-private user-library-read';
-
 function App() {
   const userLogin = () => {
-    const URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}'&response_type=code&redirect_uri=${encodeURIComponent(
-      REDIRECT_LINK
-    )}&scope=${encodeURIComponent(SCOPE)}`;
-    window.location.href = URL;
+    window.location.href = "https://witty-plant-067c6f61e.6.azurestaticapps.net/callback";
   }
 
   return (
@@ -22,3 +15,4 @@ function App() {
 }
 
 export default App
+
